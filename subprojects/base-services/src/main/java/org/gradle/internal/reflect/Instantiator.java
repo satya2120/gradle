204cfe26e7e2c2d9004708b7c3c144a8125329bb
Adam Murdoch <adam@gradle.com>
2020-09-02 15:32:49 +1000

    Remove some special case handling of services in configuration cache serialization, replace with `@ServiceScope` annotations.

--- subprojects/base-services/src/main/java/org/gradle/internal/reflect/Instantiator.java
+++ subprojects/base-services/src/main/java/org/gradle/internal/reflect/Instantiator.java
@@ -16,6 +16,8 @@
 package org.gradle.internal.reflect;
 
 import org.gradle.api.reflect.ObjectInstantiationException;
+import org.gradle.internal.service.scopes.Scopes;
+import org.gradle.internal.service.scopes.ServiceScope;
 
 /**
  * An object that can create new instances of various types. An {@code Instantiator}, depending on its implementation and configuration, may provide
@@ -34,6 +36,7 @@ import org.gradle.api.reflect.ObjectInstantiationException;
  *
  * <p>A service of this type is available in all scopes. However, the recommended way to receive an {@code Instantiator} is via a {@link org.gradle.internal.instantiation.InstantiatorFactory}.</p>
  */
+@ServiceScope(Scopes.BuildTree.class)
 public interface Instantiator {
 
     /**
